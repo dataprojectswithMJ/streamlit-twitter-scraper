d898b79bfcd7b5dae18dac0f5950155decd32435
Mpho J Kubeka <kubeka7712@gmail.com>
2021-10-19 13:29:11 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,11 @@
+import base64
+
+def get_csv_download_link(csv, filename):
+    """Generates a link allowing the data in a given panda dataframe to be downloaded
+    in:  dataframe
+    out: href string
+    """
+
+    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
+    href = f'<a href="data:file/csv;base64,{b64}" download="{filename}.csv">Download csv file</a>'
+    return href
